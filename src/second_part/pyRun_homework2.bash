2c2ea55e8abc4230489a93b0160afcc9a7377268
nontrie <komkanin.m@gmail.com>
2025-09-20 03:29:49 +0700

    Update pyRun_homework2.bash

--- src/second_part/pyRun_homework2.bash
+++ src/second_part/pyRun_homework2.bash
@@ -2,13 +2,11 @@
 # build workspace
 colcon build --symlink-install
 
-# source setup (important!)
 source install/setup.bash
 
-# reset turtlesim
+# setup turtlesim
 ros2 service call /reset std_srvs/srv/Empty {}
 ros2 service call /turtle1/teleport_absolute turtlesim/srv/TeleportAbsolute "{x: 2.0, y: 8.0, theta: -1.57}"
 ros2 service call /clear std_srvs/srv/Empty {}
 
-# run your node
 ros2 run second_part Writer
